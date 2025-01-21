db013bf35525010aad23b2123fd5fda6aaa32043
Vijay Kumar <Be1newinner@gmail.com>
2025-01-21 15:26:00 +0530

    frontend: First Push Made

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home, } from "./Pages";
+import Login from "./app/Login/Login";
+import Signup from "./app/Login/Signup";
+import TripBooking from "./Pages/TripBooking/index";
+import TourPackage from "./Pages/TourPackage";
+import PrivacyPage from "./Pages/Page/index";
+import ContactUs from "./Pages/Page/contactUs";
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/TripBooking" element={<TripBooking />}/>
+        <Route path="/TourPackage" element={<TourPackage/>}/>
+        <Route path="/privacyPage" element={<PrivacyPage/>}/>
+        <Route path="/contact" element={<ContactUs/>}/>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
