889862e03d69398cfdc92f1aeebc048c3d4d678e
Vijay Kumar <Be1newinner@gmail.com>
2025-07-02 11:05:18 +0530

    feat: integrate Clarity for enhanced analytics in Providers component; remove redundant Clarity initialization from layout

--- apps/client_frontend/src/app/Providers.tsx
+++ apps/client_frontend/src/app/Providers.tsx
@@ -1,19 +1,25 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import Clarity from "@microsoft/clarity";
 
 const queryClient = new QueryClient();
 
 type ProvidersProps = {
-    children: React.ReactNode;
+  children: React.ReactNode;
 };
 
-const Providers: React.FC<ProvidersProps> = ({ children }) => (
+const Providers: React.FC<ProvidersProps> = ({ children }) => {
+    useEffect(()=>{
+        Clarity.init("s8gd2xbff6");
+    },[])
+  return (
     <QueryClientProvider client={queryClient}>
-        {children}
-        <ReactQueryDevtools initialIsOpen={false} />
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
-);
+  );
+};
 
 export default Providers;
--- apps/client_frontend/src/app/layout.tsx
+++ apps/client_frontend/src/app/layout.tsx
@@ -2,9 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Providers from "./Providers";
-import Clarity from "@microsoft/clarity";
 
-Clarity.init("s8gd2xbff6");
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
